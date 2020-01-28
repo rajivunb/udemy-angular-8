7d44ccc70b31776d4cca5c5f5c10485689c89109
Rajiv Geeverghese <rajiv.unb@gmail.com>
2020-01-27 21:07:12 -0300

    aula 160

--- basic-recipes/src/app/header/header.component.ts
+++ basic-recipes/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -6,11 +6,4 @@ import { Component, EventEmitter, Output } from '@angular/core';
     styleUrls: ['./header.component.css']
 })
 
-export class HeaderComponent {
-
-  @Output() menuSelected = new EventEmitter<string>();
-
-  onSelect(menuItem: string) {
-    this.menuSelected.emit(menuItem);
-  }
-}
+export class HeaderComponent {}
