4a07d071fc3161c0898138060190bab59fcd59e7
Rajiv Geeverghese <rajiv.unb@gmail.com>
2020-02-02 19:13:55 -0300

    aula 190

--- basic-recipes/src/app/app.component.ts
+++ basic-recipes/src/app/app.component.ts
@@ -5,11 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'basic-recipes';
-  loadedMenuItem = 'recipe';
-
-  onNavigate(menuItem: string) {
-    this.loadedMenuItem = menuItem;
-  }
-}
+export class AppComponent {}
--- basic-recipes/src/app/shopping-list/shopping-list.service.ts
+++ basic-recipes/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,9 @@
 import { Ingredient } from '../shared/ingredient.model';
-import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 
 export class ShoppingListService {
 
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -12,7 +12,7 @@ export class ShoppingListService {
 
   addIngredient(ing: Ingredient) {
     this.ingredients.push(ing);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   getIngredients() {
@@ -27,6 +27,6 @@ export class ShoppingListService {
     // usa o spread operator (...) para converter um array em uma
     // lista de elementos
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
