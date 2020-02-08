d7d7e389928bb9db1313713ddf0d33b1ab60d34d
Rajiv Geeverghese <rajiv.unb@gmail.com>
2020-02-08 16:45:59 -0300

    aula 200

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,9 +9,29 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
 
   @ViewChild('f', {static: true}) signupForm: NgForm;
+  @ViewChild('fTask', {static: true}) taskForm: NgForm;
+  answer = '';
+  genders = ['male', 'female'];
+  submitted = false;
+  subscriptions = ['basic', 'advanced', 'pro'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {username: suggestedName, email: ''},
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
+    this.signupForm.form.patchValue({userData: {username: suggestedName}});
   }
 
   // onSubmit(form: NgForm) {
@@ -19,6 +39,17 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;    
+    
+    this.signupForm.reset();
+  }
+
+  onSubmitTask() {
+    console.log(this.taskForm);    
   }
 }
